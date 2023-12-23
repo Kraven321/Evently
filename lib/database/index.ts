0b8c1d25d71bb0ffd5fb44dcc5837a40d1aadbea
Henrique Reis <henriquereiscarvalho@outlook.com>
2023-12-23 13:55:47 -0300

    first commit

---
+++ lib/database/index.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { off } from 'process';
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || {conn: null, promise: null};
+
+export const connectToDataBase = async () => {
+    if(cached.conn) return cached.conn
+
+    if(!MONGODB_URI) throw new Error(`Mongo/URI is missing`);
+
+    cached.promisse = cached.promisse || mongoose.connect(MONGODB_URI, {
+        dbName:'Evently',
+        bufferCommands: false
+    }); 
+
+    cached.conn = await cached.promisse;
+
+    return cached.conn
+}
