9c28797370e6340f8a57608811f8a5d41d90213b
Henrique Reis <henriquereiscarvalho@outlook.com>
2024-01-05 00:07:03 -0300

    commit final

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -2,14 +2,28 @@
 
 import { CreateCategoryParams } from "@/types"
 import { handleError } from "../utils"
-import { connectToDataBase } from "../database"
+import { connectToDatabase } from "../database"
 import Category from "../database/models/category.model"
 
 export const createCategory = async ({categoryName}: CreateCategoryParams) => {
     try {
-        await connectToDataBase()
+        await connectToDatabase()
 
         const newCategory = await Category.create({name: categoryName})
+
+        return JSON.parse(JSON.stringify(newCategory))  
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export const getAllCategories = async () => {
+    try {
+        await connectToDatabase()
+
+        const categories = await Category.find()
+
+        return JSON.parse(JSON.stringify(categories))  
     } catch (error) {
         handleError(error)
     }
